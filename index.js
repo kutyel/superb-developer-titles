554e743c216dcfdc6c7c34eccc6c9331fb0dfe8c
kutyel <flavio.corparios@ext.soprasteria.com>
2018-08-28 15:09:47 +0200

    Go Native Node Modules 😅

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-require = require('esm')(module)
-module.exports = require('./main.js')
+const random = require('unique-random-array')
+const titles = require('./titles.json')
+
+const identity = x => x
+const fst = ([x, _]) => x
+const snd = ([_, x]) => x
+const constant = x => () => x
+const pickTitle = random(titles)
+const ifElse = (cond, t, f) => x => (cond(x) ? t(x) : f(x))
+const select = predicate => ifElse(constant(predicate), snd, fst)
+const extract = ({ female }) => ifElse(Array.isArray, select(female), identity)
+
+module.exports = (config = {}) => extract(config)(pickTitle())
+module.exports.fst = fst
+module.exports.snd = snd
