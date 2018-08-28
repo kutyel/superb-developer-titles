fde2ba8abdb946a6664268a8ea3e3114f46bfd13
kutyel <flavio.corparios@ext.soprasteria.com>
2018-08-28 11:03:30 +0200

    ESModules ftw! 🚀

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-'use strict'
-
-const random = require('unique-random-array')
-const titles = require('./titles.json')
-
-const fst = ([x, _]) => x
-const snd = ([_, x]) => x
-const compose = (f, g) => x => f(g(x))
-const dupe = x => (Array.isArray(x) ? x : [x, x])
-const ifElse = (cond, t, f) => x => (cond ? t(x) : f(x))
-
-module.exports = (config = {}) =>
-  random(
-    titles.map(
-      compose(
-        ifElse(config.female, snd, fst),
-        dupe
-      )
-    )
-  )()
+require = require('esm')(module)
+module.exports = require('./main.js')
--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 import test from 'ava'
-import getTitle from '.'
+import getTitle, { fst, snd } from '.'
+
+import titles from './titles.json'
+
+const dupe = x => (Array.isArray(x) ? x : [x, x])
+const dupeTitles = titles.map(dupe)
 
 test('main', t => {
   const first = getTitle()
   t.true(first.length > 0)
   t.not(first, getTitle(), 'should never repeat titles')
-  // TODO: prove test for female random titles (!?)
+})
+
+test('config({female: true})', t => {
+  t.not(dupeTitles.map(fst).includes(getTitle({ female: true })))
+  t.true(dupeTitles.map(snd).includes(getTitle({ female: true })))
 })
