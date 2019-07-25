891f0b4aa5fe4be24441575dbf2ff86b636c2312
kutyel <flaviocorpa@gmail.com>
2019-07-25 10:02:02 +0200

    Fix package

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const random = require('unique-random-array')
 const titles = require('./titles.json')
 
-const identity = x => x
 const fst = ([x, _]) => x
 const snd = ([_, x]) => x
 const constant = x => () => x
-const pickTitle = random(titles)
+const when = (cond, f) => ifElse(cond, f, x => x)
 const ifElse = (cond, t, f) => x => (cond(x) ? t(x) : f(x))
 const select = predicate => ifElse(constant(predicate), snd, fst)
-const extract = ({ female }) => ifElse(Array.isArray, select(female), identity)
+const extract = ({ female }) => when(Array.isArray, select(female))
 
-module.exports = (config = {}) => extract(config)(pickTitle())
+module.exports = (config = {}) => extract(config)(random(titles)())
 module.exports.fst = fst
 module.exports.snd = snd
